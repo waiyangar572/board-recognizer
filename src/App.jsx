36fbbc95efdc9b3ce338cc5d0fd2e8c1d252a5b7
waiyangar572 <t572waimath1729@gmail.com>
2025-06-25 23:10:14 +0900

    filename

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
                 <ul>
                     {(new Array(selectedFiles)).map((file, index) => (
                         <li key={index}>
-                            {file.name}
+                            {file.filename}
                             {/* <button className="remove-btn" onClick={() => handleRemoveFile(file.name)}>削除</button> */}
                         </li>
                     ))}
